fa3a67714ec920cd15a82b94ae413a0112cf1da2
adityakr1403 <52420281+adityakr1403@users.noreply.github.com>
2023-03-18 18:08:30 +0530

    key added

--- pages/project/[projectId]/index.js
+++ pages/project/[projectId]/index.js
@@ -17,14 +17,19 @@ const ProjectId = () => {
       </Head>
       <main className=" min-h-screen py-10 flex flex-col justify-center bg-teal-100">
         <div className=" text-left text-white">
-          <a target="_blank" rel="noreferrer" href={data.url} className=" mx-4 bg-violet-900 px-4 py-2 rounded-xl">
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href={data.url}
+            className=" mx-4 bg-violet-900 px-4 py-2 rounded-xl"
+          >
             Go to Github Repo
           </a>
         </div>
         <div className=" flex flex-col justify-center mx-auto px-20">
-          {data.images.map((image,index) => (
-            <div className="flex justify-center my-5 mx-4">
-              <img key={index} alt="avatar" src={image} className="rounded-md shadow-2xl" />
+          {data.images.map((image, index) => (
+            <div key={index} className="flex justify-center my-5 mx-4">
+              <img alt="avatar" src={image} className="rounded-md shadow-2xl" />
             </div>
           ))}
         </div>
